1932997761437828f9442b3ed0de5e8621739ef2
Kylie Merz <kmerz@ou.edu>
2021-02-08 14:00:08 -0600

    Started the participation on conditional statements.

--- Participations/Participation 2 - Conditional - Coin Toss/Program.cs
+++ Participations/Participation 2 - Conditional - Coin Toss/Program.cs
@@ -6,14 +6,17 @@ namespace Participation_2___Conditional___Coin_Toss
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Pick heads or tails");
-            string answer 
-                
-
+            Console.WriteLine("Guess heads or tails");
+            string answer = Console.ReadLine(); 
+           
 
             Random rand = new Random();
             int randomNbr = rand.Next(1, 3);
             Console.WriteLine($"{randomNbr}");
+            randomNbr = Convert.ToString(answer);
+
+            int headsortails = Convert.ToInt32(answer);
+            
 
         }
     }
