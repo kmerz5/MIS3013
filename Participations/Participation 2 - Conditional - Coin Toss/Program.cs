8493d1d1f2459ca60fef01750d5d639eb3bd7673
Kylie Merz <kmerz@ou.edu>
2021-02-08 13:49:59 -0600

    prompted the user to pick heads or tails

--- Participations/Participation 2 - Conditional - Coin Toss/Program.cs
+++ Participations/Participation 2 - Conditional - Coin Toss/Program.cs
@@ -6,6 +6,11 @@ namespace Participation_2___Conditional___Coin_Toss
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Pick heads or tails");
+            string answer 
+                
+
+
             Random rand = new Random();
             int randomNbr = rand.Next(1, 3);
             Console.WriteLine($"{randomNbr}");
