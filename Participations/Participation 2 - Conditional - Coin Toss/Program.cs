99882c9c22b95fc50973d54313d11e021c65a3ce
Kylie Merz <kmerz@ou.edu>
2021-02-10 13:26:24 -0600

    finished the participation with if statements

--- Participations/Participation 2 - Conditional - Coin Toss/Program.cs
+++ Participations/Participation 2 - Conditional - Coin Toss/Program.cs
@@ -6,6 +6,14 @@ namespace Participation_2___Conditional___Coin_Toss
     {
         static void Main(string[] args)
         {
+            //get the users guess
+            //convert their guess into a string or our int to a string
+            //1=heads
+            //2=tails
+            //convert the random number to heads or tails ( conditional)
+            //tell the user if they guessed correctly (conditional)
+            //output developer information
+
             Console.WriteLine("Guess heads or tails");
             string answer = Console.ReadLine(); 
            
@@ -13,9 +21,35 @@ namespace Participation_2___Conditional___Coin_Toss
             Random rand = new Random();
             int randomNbr = rand.Next(1, 3);
             Console.WriteLine($"{randomNbr}");
-            randomNbr = Convert.ToString(answer);
 
-            int headsortails = Convert.ToInt32(answer);
+            if (randomNbr == 1)
+            {
+                Console.WriteLine("The coin landed on heads");
+            }
+
+            else 
+            {
+                Console.WriteLine("The coin landed on tails");
+            }
+
+            if (answer == "heads" && randomNbr == 1)
+            {
+                Console.WriteLine("Your guess was correct");
+            }
+            else if (answer == "tails" && randomNbr == 2)
+            {
+                Console.WriteLine("Your guess was correct");
+            }
+            else
+            {
+                Console.WriteLine("Your guess was incorrect");
+            }
+
+            const string DeveloperInfo = "Kylie Merz-Student";
+
+            Console.WriteLine("The developer was" + " " + DeveloperInfo);
+
+
             
 
         }
