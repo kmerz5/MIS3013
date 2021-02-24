b7dc5b652ebb105b5ba2d88a1dab23a6dccdb7e3
Kylie Merz <kmerz@ou.edu>
2021-02-24 13:10:35 -0600

    instructions for putting a file into code and outputting that file on console

---
+++ In Class - 2-8-21/02-24-2021/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02_24_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    //Add one to each number up to 100
+            //start off with 0
+            int sum = 0;
+            for (int i = 1; i < 100; i++)
+            {
+                sum = sum + 1;
+                //sum +=i;
+            }
+            Console.WriteLine($"All the #'s added up from 1 to 100 is {sum.ToString}");
+        }
+    }
+}
---
+++ In Class - 2-8-21/File_Sample-2-24-21/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace File_Sample_2_24_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //relative path or an absolute path(no matter where this file is at, its on the computer)
+           //right click
+           //add existing
+           //filter - all files
+           //add that and down in proerties window, copy always
+
+    //CODE EXAMPLE
+            //string[] Lines = file.ReadAllLines("name of file); --> (click lightbuld and click the file you want)
+            
+
+            //string book = file.ReadAllText("name of file");
+            //console.writeLine(book);
+        }
+    }
+}
