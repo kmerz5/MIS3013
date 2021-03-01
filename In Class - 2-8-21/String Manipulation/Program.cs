016016a9a0b368e7ae38aa3f57fc470e88e7509c
Kylie Merz <kmerz@ou.edu>
2021-03-01 14:01:51 -0600

    Working on the participation - started loop to reverse the word if they put in a word that is not in the sentence.

---
+++ In Class - 2-8-21/String Manipulation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace String_Manipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message ="tAylOr SwIFT, the WeEkend, TraVis scoTT, ariana gRande, bIllie eIliSh";
+            string newMessage = message.ToUpper()[0] + message.ToLower().Substring(1);
+            //toupper and tolower are only for strings not characters
+            Console.WriteLine(newMessage);
+
+            string[] artists = message.Split(',');
+
+            for (int i = 0; i < artists.Length; i++)
+            {
+                string artist = artists[i].Trim();
+
+                string[] names = artist.Split(' ');
+                string firstName = names[0];
+                string lastName = names[1];
+
+                string newArtist = firstName.ToUpper()[0] + firstName.ToLower().Substring(1) + " " + lastName.ToUpper()[0] + lastName.ToLower().Substring(1);
+                Console.WriteLine(newArtist);
+                
+            }
+        }
+    }
+}
---
+++ Participations/(P) String Manipulation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _P__String_Manipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string statement = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.";
+            Console.WriteLine(statement);
+
+            Console.WriteLine("Choose a word from the sentences above. >>");
+            string word = Console.ReadLine();
+            if (statement.ToLower().Contains(word))
+            {
+                Console.WriteLine("What word would you change your selected word too?>>");
+                string wordChange = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Sorry I could not find your word {" + word + "}");
+                
+
+                for (int i = length - 1; i < Convert.ToInt32(word); i++)
+                {
+                    int WordCount = word.Length;
+                    Console.WriteLine()
+                    
+                }
+                
+               
+                
+            }
+           
+
+           
+
+
+
+        }
+    }
+}
