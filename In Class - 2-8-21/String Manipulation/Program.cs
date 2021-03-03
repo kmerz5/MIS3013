34124a4ad88c9a35c13054df3aece1fa36c4ea81
Kylie Merz <kmerz@ou.edu>
2021-03-03 13:11:49 -0600

    different way to do the same problem

--- In Class - 2-8-21/String Manipulation/Program.cs
+++ In Class - 2-8-21/String Manipulation/Program.cs
@@ -7,6 +7,17 @@ namespace String_Manipulation
         static void Main(string[] args)
         {
             string message ="tAylOr SwIFT, the WeEkend, TraVis scoTT, ariana gRande, bIllie eIliSh";
+            //OTHER WAY TO SOLVE THIS / this is hardcoding the values so it wont work for The Weekend
+            string msg2 = "tAyLor SwiFt";
+
+            //Help with participation putting the names vertically not horizontally
+           // for (int i = 0; i < msg2.Length; i++)
+           // {
+               // Console.WriteLine(msg2[i]);
+           // }
+
+            return;
+            
             string newMessage = message.ToUpper()[0] + message.ToLower().Substring(1);
             //toupper and tolower are only for strings not characters
             Console.WriteLine(newMessage);
@@ -16,6 +27,10 @@ namespace String_Manipulation
             for (int i = 0; i < artists.Length; i++)
             {
                 string artist = artists[i].Trim();
+
+                int INDEXOFSPACE = msg2.IndexOf(' '); //LOOKING FOR A SPACE
+                msg2 = msg2.ToUpper()[0] + msg2.ToLower().Substring(1, INDEXOFSPACE) + msg2.ToUpper()[INDEXOFSPACE + 1] + msg2.ToLower().Substring(INDEXOFSPACE + 2);
+                Console.WriteLine(msg2);
 
                 string[] names = artist.Split(' ');
                 string firstName = names[0];
