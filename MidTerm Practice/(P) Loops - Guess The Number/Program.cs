87bdf9de71e038128e6e97755c267eb8339b0960
Kylie Merz <kmerz@ou.edu>
2021-03-08 14:27:44 -0600

    finished the loops - guess the number and added in the TryParse aspect.

--- MidTerm Practice/(P) Loops - Guess The Number/Program.cs
+++ MidTerm Practice/(P) Loops - Guess The Number/Program.cs
@@ -13,6 +13,62 @@ namespace _P__Loops___Guess_The_Number
              * prompt them to input a valid number 
              * (Hint: https://docs.microsoft.com/en-us/dotnet/api/system.int32.tryparse?view=netcore-3.1 
              * (Links to an external site.) )*/
+
+            Console.WriteLine("Pick the max value>>");
+            string answer = Console.ReadLine();
+            int max;
+            if (int.TryParse(answer, out max) == true)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("This is not a valid input! GoodBye.");
+                Environment.Exit(-1);
+            }
+
+            Console.WriteLine("Pick the min value>>");
+            answer = Console.ReadLine();
+            int min;
+            if (int.TryParse(answer, out min) == true)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("This is not a valid input! Goodbye.");
+                Environment.Exit(-1);
+            }
+
+            Random rand = new Random();
+            int RandomNumber = rand.Next(min, max);
+
+            Console.WriteLine("Guess the random number that will be between " + min + " and " + max);
+            string guess = Console.ReadLine();
+            int GUESS;
+            if (int.TryParse(guess,out GUESS)==true)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("That is not a valid input. Goodbye.");
+                Environment.Exit(-1);
+            }
+
+            while (GUESS != RandomNumber)
+            {
+                Console.WriteLine("Guess again!");
+                guess = Console.ReadLine();
+                GUESS = Convert.ToInt32(guess);
+            }
+            if (GUESS == RandomNumber)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("Good job!");
+            }
+
+
         }
     }
 }
