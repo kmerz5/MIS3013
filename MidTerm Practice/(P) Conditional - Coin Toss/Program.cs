bde3a1e67e965780d47e3727125bbce4181d2fe9
Kylie Merz <kmerz@ou.edu>
2021-03-08 12:20:02 -0600

    Finished Coin Toss participation

--- MidTerm Practice/(P) Conditional - Coin Toss/Program.cs
+++ MidTerm Practice/(P) Conditional - Coin Toss/Program.cs
@@ -13,6 +13,40 @@ namespace _P__Conditional___Coin_Toss
              * Tell the user what side the coin landed on (heads or tails) and then whether or 
              * not their guess was right or wrong. 
              * Display the developer name (store as a constant) before the user exits the application.*/
+
+            Console.WriteLine("Pick heads or tails>>");
+            string answer = Console.ReadLine();
+
+            Random Rand = new Random();
+            int RandNumber = Rand.Next(1, 3);
+
+            if (RandNumber == 1)
+            {
+                Console.WriteLine("The coin landed on heads.");
+            }
+            else
+            {
+                Console.WriteLine("The coin landed on heads.");
+            }
+
+            if (answer.ToLower() == "heads" && RandNumber == 1)
+            {
+                Console.WriteLine("Your guess was correct!");
+            }
+            else if (answer.ToLower() == "tails" && RandNumber == 2)
+            {
+                Console.WriteLine("Your guess was correct!");
+            }
+            else
+            {
+                Console.WriteLine("Your guess was incorrect.");
+            }
+
+            const string DeveloperInfo = "Kylie Merz - Student";
+            Console.WriteLine(" ");
+            Console.WriteLine("Developer Info: " + DeveloperInfo);
+
+
         }
     }
 }
