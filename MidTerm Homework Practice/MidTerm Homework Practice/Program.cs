2c67a7002bd2966dacc1e12e677af06c8e03a890
Kylie Merz <kmerz@ou.edu>
2021-03-09 16:28:21 -0600

    Finished homework 1 part 2

--- MidTerm Homework Practice/MidTerm Homework Practice/Program.cs
+++ MidTerm Homework Practice/MidTerm Homework Practice/Program.cs
@@ -13,7 +13,7 @@ namespace MidTerm_Homework_Practice
             to the user.  Assume that the sales tax for your application is 8.5% (create a constant to store this 
             value and use the constant in your calculations)*/
 
-            Console.WriteLine("What type of item are you purchasing?");
+           /* Console.WriteLine("What type of item are you purchasing?");
             string type = Console.ReadLine();
 
             Console.WriteLine("How many " + type + "'s are you purchasing?");
@@ -32,17 +32,56 @@ namespace MidTerm_Homework_Practice
             Console.WriteLine(" ");
             Console.WriteLine("Your subtotal is " + subtotal.ToString("C2"));
             Console.WriteLine("Your SalesTax is " + SalesTax.ToString("C2"));
-            Console.WriteLine("Your SalesTotal is " + SalesTotal.ToString("C2"));
-             
+            Console.WriteLine("Your SalesTotal is " + SalesTotal.ToString("C2"));*/
 
 
 
+            /*Student Grade
+                Create a Visual C# Windows Console Application that will prompt the user to enter their first name, 
+            their last name, their student id number as well as their overall percentage grade for quizzes, 
+            participations, homeworks, midterm and final exam.  Use the percentages from this class to calculate 
+            their final grade and output it as a percent*/
 
-        /*Student Grade
-            Create a Visual C# Windows Console Application that will prompt the user to enter their first name, 
-        their last name, their student id number as well as their overall percentage grade for quizzes, 
-        participations, homeworks, midterm and final exam.  Use the percentages from this class to calculate 
-        their final grade and output it as a percent*/
-}
+            Console.WriteLine("What is your first name?");
+            string FirstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            string LastName = Console.ReadLine();
+
+            Console.WriteLine("What is your student ID number?");
+            string answer = Console.ReadLine();
+            double ID = Convert.ToDouble(answer);
+
+            Console.WriteLine("What was your overall grade for participations?");
+            answer = Console.ReadLine();
+            double participation = Convert.ToDouble(answer);
+
+            Console.WriteLine("What was your overall grade for homeworks?");
+            answer = Console.ReadLine();
+            double homework = Convert.ToDouble(answer);
+
+            Console.WriteLine("What was your overall grade for the midterm?");
+            answer = Console.ReadLine();
+            double midterm = Convert.ToDouble(answer);
+
+            Console.WriteLine("What was your overall grade for the final exam?");
+            answer = Console.ReadLine();
+            double final = Convert.ToDouble(answer);
+
+            double part = .08;
+            double home = .12;
+            double mid = .40;
+            double Fi = .40;
+
+            double partgrade = (participation * part)/100;
+            double homegrade = (homework * home) / 100;
+            double midgrade = (midterm * mid) / 100;
+            double finalgrade = (final * Fi) / 100;
+
+            double OverAllPercentage = partgrade + homegrade + midgrade + finalgrade;
+            Console.WriteLine("Your overall grade for this course is " + OverAllPercentage.ToString("P2"));
+
+
+        }
 }
 }
