d4a688b96aa3f47c51d5135c3a9ff2f33c79cafa
Kylie Merz <kmerz@ou.edu>
2021-03-09 16:13:25 -0600

    Finished homework 1 part 1

--- MidTerm Homework Practice/MidTerm Homework Practice/Program.cs
+++ MidTerm Homework Practice/MidTerm Homework Practice/Program.cs
@@ -8,10 +8,32 @@ namespace MidTerm_Homework_Practice
         {
             /*Sale Total
                 Create a Visual C# Windows Console Application that will prompt the user to enter what
-            type of item they are purchasing, the quantity of the item, and the price for the item.  
+            type of item they are purchasing,/ the quantity of the item, and the price for the item.DONE  
             Calculate the subtotal, the sales tax and the sales total (subtotal + sales tax) and output all 3 
             to the user.  Assume that the sales tax for your application is 8.5% (create a constant to store this 
             value and use the constant in your calculations)*/
+
+            Console.WriteLine("What type of item are you purchasing?");
+            string type = Console.ReadLine();
+
+            Console.WriteLine("How many " + type + "'s are you purchasing?");
+            string quantity = Console.ReadLine();
+            int Count = Convert.ToInt32(quantity);
+
+            Console.WriteLine("What is the price for this item?");
+            string answer = Console.ReadLine();
+            int price = Convert.ToInt32(answer);
+
+            int subtotal = price * Count;
+            double Tax = .085;
+            double SalesTax = subtotal * Tax;
+            double SalesTotal = subtotal + SalesTax;
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Your subtotal is " + subtotal.ToString("C2"));
+            Console.WriteLine("Your SalesTax is " + SalesTax.ToString("C2"));
+            Console.WriteLine("Your SalesTotal is " + SalesTotal.ToString("C2"));
+             
 
 
 
