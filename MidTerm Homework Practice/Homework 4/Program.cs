a285614b27186cb1246302801c41667a8bcb2f17
Kylie Merz <kmerz@ou.edu>
2021-03-09 21:03:40 -0600

    Finished homework 4 part 2

--- MidTerm Homework Practice/Homework 4/Program.cs
+++ MidTerm Homework Practice/Homework 4/Program.cs
@@ -12,37 +12,37 @@ namespace Homework_4
             and vowels in the following format (2 separate lines and the format specifier should be general with 
             0 decimal places!)*/
 
-            Console.WriteLine("Please input a sentence.");
-            string sentence = Console.ReadLine();
-            sentence = sentence.ToLower();
+            /* Console.WriteLine("Please input a sentence.");
+             string sentence = Console.ReadLine();
+             sentence = sentence.ToLower();
 
-            int vowels = 0;
-            int consonants = 0;
+             int vowels = 0;
+             int consonants = 0;
 
-            for (int i = 0; i < sentence.Length; i++)
-            {
-                char letter = sentence[i];
-                if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
-                {
-                vowels++;
-                }
-                else if ((letter == ' ' || letter == '!' || letter == '?' || letter == '\"' || letter == '\'' ||
-                    letter == '1' || letter == '2' || letter == '3' || letter == '4' || letter == '5' ||
-                    letter == '6' || letter == '7' || letter == '8' || letter == '9' || letter == ';' ||
-                    letter == ':' || letter == '(' || letter == ')' || letter == '/' || letter == '&' ||
-                    letter == '.' || letter == ',' || letter == '$' || letter == '*' || letter == '#'))
-                {
+             for (int i = 0; i < sentence.Length; i++)
+             {
+                 char letter = sentence[i];
+                 if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+                 {
+                 vowels++;
+                 }
+                 else if ((letter == ' ' || letter == '!' || letter == '?' || letter == '\"' || letter == '\'' ||
+                     letter == '1' || letter == '2' || letter == '3' || letter == '4' || letter == '5' ||
+                     letter == '6' || letter == '7' || letter == '8' || letter == '9' || letter == ';' ||
+                     letter == ':' || letter == '(' || letter == ')' || letter == '/' || letter == '&' ||
+                     letter == '.' || letter == ',' || letter == '$' || letter == '*' || letter == '#'))
+                 {
 
-                }
-                else
-                {
-                    consonants++;
-                } 
-            }
-            Console.WriteLine("There were " + vowels.ToString("G0") + " vowels in your sentence.");
-            Console.WriteLine("There were " + consonants.ToString("G0") + " consonants in your sentence");
+                 }
+                 else
+                 {
+                     consonants++;
+                 } 
+             }
+             Console.WriteLine("There were " + vowels.ToString("G0") + " vowels in your sentence.");
+             Console.WriteLine("There were " + consonants.ToString("G0") + " consonants in your sentence");*/
 
-
+            //-------------------------------------------------------------------------------------------------------------------
 
             /*l337 Converter
                 Prompt the user to input a sentence.  Once you have their sentence 
@@ -61,6 +61,29 @@ namespace Homework_4
             Since we are very excited about our application, we want to make sure that every 
             sentence ends with an exclamation mark!  If it already ends with an exclamation mark, 
             do not add another, otherwise concatenate a '!' at the end!*/
-}
+
+            Console.WriteLine("Please enter a sentence.");
+            string answer = Console.ReadLine();
+            string sentence = answer.ToUpper();
+
+            sentence = sentence.Replace("A", "4");
+            sentence = sentence.Replace("E", "3");
+            sentence = sentence.Replace("H","|-|");
+            sentence = sentence.Replace("S", "$");
+            sentence = sentence.Replace("T", "7");
+            sentence = sentence.Replace("U", "|_|");
+            sentence = sentence.Replace("O", "0");
+            sentence = sentence.Replace("P", "[]D");
+
+            if (sentence[sentence.Length-1] != '!')
+            {
+                Console.WriteLine(sentence + "!");
+            }
+            else
+            {
+                Console.WriteLine(sentence);
+            }
+
+        }
 }
 }
