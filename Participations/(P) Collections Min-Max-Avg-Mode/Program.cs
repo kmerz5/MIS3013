e2b91508a899cfd654467a2acf6c6c2c9c86f328
Kylie Merz <kmerz@ou.edu>
2021-03-17 13:48:20 -0500

    asked them for their score and had it repeat untill they dont have any other exam scores

---
+++ Participations/(P) Collections Min-Max-Avg-Mode/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _P__Collections_Min_Max_Avg_Mode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> ExamGrades = new List<double>();
+            string answer;
+            do
+            {
+                Console.WriteLine("Enter your exam score. >>");
+                answer = Console.ReadLine();
+                double Useranswer = Convert.ToDouble(answer);
+
+                ExamGrades.Add(Useranswer);
+
+                Console.WriteLine("Do you have another exam score? >>");
+                answer = Console.ReadLine();
+
+            } while (answer.ToLower()[0] == 'y');
+            
+
+
+
+        }
+    }
+}
