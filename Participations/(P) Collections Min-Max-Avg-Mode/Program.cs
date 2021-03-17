3edc682b99e24ba481e0806aa1607729da77ef43
Kylie Merz <kmerz@ou.edu>
2021-03-17 15:01:04 -0500

    trying to get average to work

--- Participations/(P) Collections Min-Max-Avg-Mode/Program.cs
+++ Participations/(P) Collections Min-Max-Avg-Mode/Program.cs
@@ -8,6 +8,7 @@ namespace _P__Collections_Min_Max_Avg_Mode
         static void Main(string[] args)
         {
             List<double> ExamGrades = new List<double>();
+            List<double> AvgGrades = new List<double>();
             string answer;
             do
             {
@@ -22,14 +23,15 @@ namespace _P__Collections_Min_Max_Avg_Mode
 
             } while (answer.ToLower()[0] == 'y');
 
+
             double AverageGrade = 0;
             for (int i = 1; i <= ExamGrades.Count; i++)
             {
-                double grades = ExamGrades[i++];
-                AverageGrade = (grades + ExamGrades[i]) / ExamGrades.Count;
+                double int1 = ExamGrades[i];
+                double nextInt = i == ExamGrades.Length - 1;
                 
             }
-            Console.WriteLine("Your average score of your exams is, " + AverageGrade);
+            Console.WriteLine("Your average score of your exams is: " + AverageGrade);
 
 
 
