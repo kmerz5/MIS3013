5eaf8b4399a272b9a62e6a4f804af6f7d4140352
Kylie Merz <kmerz@ou.edu>
2021-04-07 13:05:19 -0500

    finished partcipation

---
+++ Participations/(P) Speak/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _P__Speak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please choose an animal: dog, monkey, or goat. >>");
+            string answer = Console.ReadLine();
+            string Output = speak(answer);
+        }
+
+        static string speak(string answer)
+        {
+            if (answer.ToLower() == "dog")
+            {
+                Console.WriteLine("The sound a " + answer + " makes is ruff!");
+
+            }
+            else if (answer.ToLower() == "monkey")
+            {
+                Console.WriteLine("The sound a " + answer + " makes is oohoohaahaah!");
+
+            }
+            else if (answer.ToLower() == "goat")
+            {
+                Console.WriteLine("The sound a " + answer + " makes is bahahahahaa!");
+
+            }
+            else
+            {
+                Console.WriteLine("The sound a(n) " + answer + " makes is merrrrrrrrp!");
+            }
+            return answer;
+
+        }
+}
+    
+}
