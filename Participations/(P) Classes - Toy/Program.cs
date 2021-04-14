381d687742f69afdd19cbb9813d5b22d06a10d87
Kylie Merz <kmerz@ou.edu>
2021-04-14 13:32:34 -0500

    Created the class "Toy"

---
+++ Participations/(P) Classes - Toy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _P__Classes___Toy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Participations/(P) Classes - Toy/Toy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _P__Classes___Toy
+{
+    class Toy
+    {
+//ADDING PROPERTIES____________________________________________________________________________
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        private string Notes; 
+
+//ADDING METHODS___________________________________________________________________________
+        public Toy()
+        {
+            Manufacturer = "Gerber";
+            Name = "Kylie Merz";
+            Price = 20.00;
+            Notes = " ";
+
+        }
+
+        public string GetAisle()
+        {
+            char FirstLetter = Manufacturer.ToUpper()[0];
+            Random rand = new Random();
+            int randomnumber = rand.Next(1,24);
+            string LetterNumber = FirstLetter + randomnumber.ToString();
+            return LetterNumber;
+        }
+    }
+}
+ 
