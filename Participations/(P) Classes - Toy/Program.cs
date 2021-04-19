38154ba8090a432b62a9222d6949c3dbbbdb423d
Kylie Merz <kmerz@ou.edu>
2021-04-19 13:13:03 -0500

    Fixed (P) Toy

--- Participations/(P) Classes - Toy/Program.cs
+++ Participations/(P) Classes - Toy/Program.cs
@@ -6,13 +6,24 @@ namespace _P__Classes___Toy
     {
         static void Main(string[] args)
         {
-            Toy toy1 = new Toy("Good for children under the age of 10.");
+            Toy toy1 = new Toy("For ages 5+");
             toy1.Manufacturer = "disney";
             toy1.Name = "Princess";
             toy1.Price = 24.00;
+            
+            
 
-            string sentence = toy1.GetAisle();
+            string sentence = toy1.Name + " is mannufactured by " + toy1.Manufacturer + " and costs, " + toy1.Price.ToString("C2");
             Console.WriteLine($"Toy 1: {sentence} ");
+
+            Toy toy2 = new Toy("Do not eat");
+            toy2.Name = "bubbles";
+            toy2.Manufacturer = "Bubble Company";
+            toy2.Price = 4.99;
+
+            sentence = toy2.Name + " is mannufactured by " + toy2.Manufacturer + " and costs, " + toy2.Price.ToString("C2");
+            Console.WriteLine($"Toy 2: {sentence} ");
+
         }
     }
 }
--- Participations/(P) Classes - Toy/Toy.cs
+++ Participations/(P) Classes - Toy/Toy.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace _P__Classes___Toy
 {
-    class Toy
+    public class Toy
     {
 //ADDING PROPERTIES____________________________________________________________________________
         public string Manufacturer { get; set; }
@@ -13,28 +13,37 @@ namespace _P__Classes___Toy
         private string Notes; 
 
 //ADDING METHODS___________________________________________________________________________
+        //This is our constructor
         public Toy()
         {
-            Manufacturer = "Gerber";
-            Name = "Kylie Merz";
-            Price = 20.00;
-            Notes = " ";
+            Manufacturer = string.Empty;
+            Name = string.Empty;
+            Price = 0;
+            Notes = string.Empty;
 
         }
 
         public Toy(string notes)
         {
             Notes = notes;
+            Manufacturer = string.Empty;
+            Name = string.Empty;
+            Price = 0;
             
+
         }
 
         public string GetAisle()
         {
-            char FirstLetter = Manufacturer.ToUpper()[0];
+            string aisle = "";
             Random rand = new Random();
-            int randomnumber = rand.Next(1,24);
-            string LetterNumber = FirstLetter + randomnumber.ToString();
-            return LetterNumber;
+            aisle = Manufacturer.ToUpper()[0].ToString() + rand.Next(1,25);
+            //char FirstLetter = Manufacturer.ToUpper()[0];
+            //Random rand = new Random();
+            //int randomnumber = rand.Next(1,24);
+            //string LetterNumber = FirstLetter + randomnumber.ToString();
+            //return LetterNumber;
+            return aisle;
         }
     }
 }
