e15e0fe65ae37353545fc65eed99e0260cfd6226
Kylie Merz <kmerz@ou.edu>
2021-05-07 15:10:41 -0500

    Finished (P) Cereal

---
+++ Final Extra Practice/Classes - Cereal/Cereal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes___Cereal
+{
+    public class Cereal
+    {
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Calories { get; set; }
+        public double Cups { get; set; }
+
+        public Cereal()
+        {
+            Manufacturer = string.Empty;
+            Name = string.Empty;
+            Calories = 0.0;
+            Cups = 0.0;
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} made by {Manufacturer} contains {Calories.ToString("N1")} calories per serving and {Cups.ToString("N1")} cups per serving.";
+        }
+    }
+
+}
--- Final Extra Practice/Classes - Cereal/Program.cs
+++ Final Extra Practice/Classes - Cereal/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Classes___Cereal
 {
@@ -6,7 +8,61 @@ namespace Classes___Cereal
     {
         static void Main(string[] args)
         {
-            //Look on canvas for the UML
+            /*We want to read in a data set of Cereal information and help answer a few questions for the user.  
+             * The application should output all of the Cereal information that have a serving size that is 1 cup 
+             * or more.  After you output all those cereals, we also need to inform them which Cereals have 100 
+             * calories or less per serving.*/
+
+            //data set that was input was separated by |
+
+            //Read in dataset
+            string[] lines = File.ReadAllLines("Cereal_Data (1).txt");
+
+            //create a list to contain all the lines of the file
+            List<Cereal> bowls = new List<Cereal>();
+
+            //Go through the list and split at the | and add each column to each property of the class
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+
+                string[] CerealInformation = line.Split('|');
+
+                Cereal c = new Cereal();
+                c.Manufacturer = CerealInformation[0];
+                c.Name = CerealInformation[1];
+                c.Calories = Convert.ToDouble(CerealInformation[2]);
+                c.Cups = Convert.ToDouble(CerealInformation[3]);
+
+                bowls.Add(c);
+
+            }
+
+            //Outputting all the cereal information where the serving size is 1 cup
+            Console.WriteLine("Here is all the cereal information for cereals which have a serving size less than 1 cup:");
+            Console.WriteLine(" ");
+            foreach (Cereal CerealBowls in bowls)
+            {
+                if (CerealBowls.Cups > 1)
+                {
+                    Console.WriteLine(CerealBowls);
+
+                }
+
+            }
+            //outputting all the cereal information where the serving size has 100 calories or less
+            Console.WriteLine(" ");
+            Console.WriteLine("Here is all the cereal information for cereals which have 100 or less calories per serving:");
+            Console.WriteLine(" ");
+            foreach (Cereal CB in bowls)
+            {
+                if (CB.Calories <= 100)
+                {
+                    Console.WriteLine(CB);
+
+                }
+
+            }
         }
     }
 }
