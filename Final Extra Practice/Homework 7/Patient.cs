590d714a6a515696bbf6d6aaed1745178b002baa
Kylie Merz <kmerz@ou.edu>
2021-05-07 17:42:45 -0500

    Finished homework 7

---
+++ Final Extra Practice/Homework 7/Patient.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_7
+{
+   public class Patient
+    {
+        //Properties
+        public string FirstName { get; set; }
+        public char MidInitial { get; set; }
+        public string LastName { get; set; }
+        public int ID { get; set; }
+        public bool FluVaccineYN { get; set; }
+        public Dictionary<DateTime, double> Weights { get; set; }
+        public List<string> AppointmentHistory { get; set; }
+
+
+     //Methods
+
+        //Empty Constructor
+        public Patient()
+        {
+            FirstName = string.Empty;
+            MidInitial = ' ';
+            LastName = string.Empty;
+            ID = 0;
+            FluVaccineYN = false;
+            Weights = new Dictionary<DateTime, double>();
+            AppointmentHistory = new List<string>();
+        }
+
+        //Constructor but that passes through int patID
+        public Patient(int patID)
+        {
+            FirstName = string.Empty;
+            MidInitial = ' ';
+            LastName = string.Empty;
+            ID = patID;
+            FluVaccineYN = false;
+            Weights = new Dictionary<DateTime, double>();
+            AppointmentHistory = new List<string>();
+
+        }
+
+        //Constructor that passes through firstname, midinitial, and lastname
+        public Patient(string firstName, char midInit, string lastName)
+        {
+            FirstName = firstName;
+            MidInitial = midInit;
+            LastName = lastName;
+            ID = 0;
+            FluVaccineYN = false;
+            Weights = new Dictionary<DateTime, double>();
+            AppointmentHistory = new List<string>();
+
+        }
+
+        //To String
+        public override string ToString()
+        {
+            string toOutPut = $"{LastName}, {FirstName}, {MidInitial}. ({ID}), Flue Vaccine: ";
+            if (FluVaccineYN)
+            {
+                toOutPut += "Yes";
+
+            }
+            else
+            {
+                toOutPut += "No";
+            }
+            return toOutPut;
+        }
+
+        public double CalculateMaxWeight()
+        {
+            double maxweight = -50;
+            foreach (double weight in Weights.Values)
+            {
+                if (maxweight <= weight)
+                {
+                    maxweight = weight;
+                }
+
+            }
+            return maxweight;
+        }
+
+        public void DisplayAppt()
+        {
+            foreach (string appointment in AppointmentHistory)
+            {
+                Console.WriteLine(appointment);
+
+            }
+        }
+
+        public void AdministerVaccine()
+        {
+            if (FluVaccineYN)
+            {
+                Console.WriteLine("This patient has already had the vaccine.");
+
+            }
+            else
+            {
+                Console.WriteLine("The patient has now been administered the vaccine.");
+                FluVaccineYN = true;
+            }
+        }
+
+    }
+}
--- Final Extra Practice/Homework 7/Program.cs
+++ Final Extra Practice/Homework 7/Program.cs
@@ -6,7 +6,27 @@ namespace Homework_7
     {
         static void Main(string[] args)
         {
-            //look on Canvas for the problem for the UML
+            /*You are the manager of a college health center. You decide that it is a good idea to develop 
+             * an online health portal where patients can view their information (test results, appointments, etc.).
+             * Before you can progress the project, you need to identify which patient attributes your system will 
+             * track and then create a model.*/
+
+            Console.WriteLine("Patient 1:");
+            Console.WriteLine(" ");
+            Patient p1 = new Patient();
+            p1.FirstName = "Kylie";
+            p1.MidInitial = 'E';
+            p1.LastName = "Merz";
+            p1.ID = 5;
+            p1.FluVaccineYN = false;
+            p1.Weights.Add(DateTime.Now, 130);
+            p1.Weights.Add(DateTime.Now.AddDays(-400),140);
+            p1.AppointmentHistory.Add("Yearly Checkup");
+            Console.WriteLine(p1);
+            p1.AdministerVaccine();
+            Console.WriteLine(p1);
+            Console.WriteLine($"{p1.ToString()}, Max Weight: {p1.CalculateMaxWeight()}");
+
         }
     }
 }
