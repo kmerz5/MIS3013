7218b8ba520db3a2796b40d819bca9a039b27131
Kylie Merz <kmerz@ou.edu>
2021-05-10 17:14:47 -0500

    Finished homework 7

---
+++ Final Extra Practice/yHomework 7/Patient.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace yHomework_7
+{
+   public class Patient
+    {
+        //properties
+        public string FirstName { get; set; }
+        public char MidInitial { get; set; }
+        public string LastName { get; set; }
+        public int ID { get; set; }
+        public bool FluVaccineYN { get; set; }
+        public Dictionary<DateTime, double> Weights { get; set; }
+        public List<string> AppointmentHistory { get; set; }
+
+        //constructors
+        public Patient()
+        {
+            FirstName = string.Empty;
+            MidInitial = ' ';
+            LastName = string.Empty;
+            ID = 0;
+            FluVaccineYN = false;
+           Weights = new Dictionary<DateTime, double>();
+            AppointmentHistory = new List<string>();
+        }
+
+        public Patient(int patID)
+        {
+            FirstName = string.Empty;
+            MidInitial = ' ';
+            LastName = string.Empty;
+            ID = patID;
+            FluVaccineYN = false;
+            Weights = new Dictionary<DateTime, double>();
+            AppointmentHistory = new List<string>();
+
+        }
+
+        public Patient(string firstName, char midInit, string lastName)
+        {
+            FirstName = firstName;
+            MidInitial = midInit;
+            LastName = lastName;
+            ID = 0;
+            FluVaccineYN = false;
+           Weights =  new Dictionary<DateTime, double>();
+            AppointmentHistory = new List<string>();
+        }
+
+        public override string ToString()
+        {
+            string output = $"{LastName},{FirstName} {MidInitial}. ({ID}) Flu Vaccine: ";
+            if (FluVaccineYN)
+            {
+                output += "yes";
+
+            }
+            else
+            {
+                output += "no";
+            }
+            return output; 
+        }
+
+        public double CalculateMaxWeight()
+        {
+            double maxweight = -50;
+            foreach (double weight in Weights.Values)
+            {
+                if (weight > maxweight)
+                {
+                    maxweight = weight;
+
+                }
+
+            }
+            return maxweight;
+        }
+
+        public void DisplayApptHist()
+        {
+            foreach (string appointment in AppointmentHistory)
+            {
+                Console.WriteLine($"{appointment}");
+            }
+        }
+
+        public void AdministerVaccine()
+        {
+            if (FluVaccineYN)
+            {
+                Console.WriteLine("This patient has already received the flu vaccine.");
+
+            }
+            else
+            {
+                Console.WriteLine("This patient has now been administered the flu vaccine.");
+                FluVaccineYN = true;
+            }
+        }
+    }
+}
---
+++ Final Extra Practice/yHomework 7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace yHomework_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Patient p1 = new Patient();
+            p1.FirstName = "Kylie";
+            p1.MidInitial = 'E';
+            p1.LastName = "Merz";
+            p1.ID = 5;
+            p1.FluVaccineYN = false;
+            p1.Weights.Add(DateTime.Now, 120);
+            p1.Weights.Add(DateTime.Now.AddDays(-145), 140);
+            p1.AppointmentHistory.Add("Annual Checkup");
+            p1.AppointmentHistory.Add("Sports Physical");
+
+            Console.WriteLine(p1);
+            p1.AdministerVaccine();
+            Console.WriteLine(p1);
+            Console.WriteLine($"Max Weight: {p1.CalculateMaxWeight()}");
+            Console.WriteLine("Appointment History: ");
+            p1.DisplayApptHist();
+            
+            
+        }
+    }
+}
