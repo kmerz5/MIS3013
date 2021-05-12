8001fd1a965b1cd7fb1a38fc4c3317d09dc4f69a
Kylie Merz <kmerz@ou.edu>
2021-05-11 20:27:35 -0500

    did speak

---
+++ Final Extra Practice/zSpeak/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace zSpeak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            Console.WriteLine("Please choose a type of animal.>>");
+            answer = Console.ReadLine();
+            Speak(answer);
+
+        }
+
+        static string Speak(string answer)
+        {
+            if (answer.ToLower() == "dog")
+            {
+                Console.WriteLine("The sound a dog makes is 'woof'");
+
+            }
+            else if (answer.ToLower() == "monkey")
+            {
+                Console.WriteLine("The sound a monkey makes is 'oohoohaahaah'");
+
+            }
+            else if (answer.ToLower() == "goat")
+            {
+                Console.WriteLine("The sound a goat makes is 'bahahaha'");
+
+            }
+            else
+            {
+                Console.WriteLine($"The sound a {answer} makes is 'ghghghgh'");
+            }
+            return answer;
+        }
+    }
+}
