d99776f6aa897db43edeadf7cde5ebec3049daf9
Kylie Merz <kmerz@ou.edu>
2021-05-11 21:39:39 -0500

    Finished homework 7

---
+++ Final Extra Practice/zHomework7/Patient.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zHomework7
+{
+    public class Patient
+    {
+    //Properties
+        public string FirstName { get; set; }
+        public char MidInitial { get; set; }
+        public string LastName { get; set; }
+        public int ID { get; set; }
+        public bool FluVaccineYN { get; set; }
+        public Dictionary<DateTime, double> Weights {get;set;}
+        public List<string> AppointmentHistory { get; set; }
+
+    //constructors
+        public Patient()
+        {
+            FirstName = string.Empty;
+            MidInitial = ' ';
+            LastName = string.Empty;
+            ID = 0;
+            FluVaccineYN = false;
+            Weights = new Dictionary<DateTime, double>();
+            AppointmentHistory = new List<string>();
+        }
+
+        public Patient(int patID)
+        {
+            FirstName = string.Empty;
+            MidInitial = ' ';
+            LastName = string.Empty;
+            ID = patID;
+            FluVaccineYN = false;
+            Weights = new Dictionary<DateTime, double>();
+            AppointmentHistory = new List<string>();
+
+        }
+
+        public Patient(string firstName, char midInit, string lastName)
+        {
+            FirstName = firstName;
+            MidInitial = midInit;
+            LastName = lastName;
+            ID = 0;
+            FluVaccineYN = false;
+            Weights = new Dictionary<DateTime, double>();
+            AppointmentHistory = new List<string>();
+        }
+
+    //Override ToString()
+        public override string ToString()
+        {
+            string output = $"{LastName}, {FirstName} {MidInitial}. ({ID}) Flu Vaccine: ";
+            if (FluVaccineYN)
+            {
+                output += "yes";
+
+            }
+            else
+            {
+                output += "no";
+            }
+            return output;
+        }
+
+    //CalculateMaxWeight Method
+        public double CalculateMaxWeight()
+        {
+            double weight = -50;
+            foreach (double weights in Weights.Values)
+            {
+                if (weights > weight)
+                {
+                    weight = weights;
+
+                }
+
+
+            }
+            return weight;
+        }
+
+    //DisplayApptHist Method
+        public void DisplayApptHist()
+        {
+            foreach (string appt in AppointmentHistory)
+            {
+                Console.WriteLine(appt);
+
+            }
+            
+        }
+
+        public void AdministerVaccine()
+        {
+            if (FluVaccineYN)
+            {
+                Console.WriteLine("This patient has already received the vaccine.");
+
+            }
+            else
+            {
+                Console.WriteLine("This patient has now been administered the vaccine.");
+                FluVaccineYN = true;
+            }
+        }
+    }
+}
---
+++ Final Extra Practice/zHomework7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace zHomework7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Patient p1 = new Patient();
+            p1.FirstName = "Kylie";
+            p1.MidInitial = 'E';
+            p1.LastName = "Merz";
+            p1.ID = 5;
+            p1.FluVaccineYN = false;
+            p1.Weights.Add(DateTime.Now, 140);
+            p1.Weights.Add(DateTime.Now.AddDays(-140), 135);
+            p1.AppointmentHistory.Add("Sports Physical");
+            p1.AppointmentHistory.Add("Dentist Checkup");
+            p1.AppointmentHistory.Add("Annual checkup");
+
+            Console.WriteLine(p1);
+            p1.AdministerVaccine();
+            Console.WriteLine(p1);
+            Console.WriteLine("Appointment History:");
+            p1.DisplayApptHist();
+            Console.WriteLine(" ");
+            Console.WriteLine($"Max Weight: {p1.CalculateMaxWeight()} lbs");
+        }
+    }
+}
